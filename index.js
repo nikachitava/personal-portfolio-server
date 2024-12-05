1393d69046d89a8d61a437e7e0cf7f17f54b486e
nikachitava <chitava18.official@gmail.com>
2024-12-05 15:04:52 +0400

    connecet mongoDB db

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import exrpess from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = exrpess();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello, Express!");
-});
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log("Connected to database");
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	})
+	.catch(() => {
+		console.log("Connected failed");
+	});
 
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
-});
+// GjOJ9FlDCQJvXy6d
