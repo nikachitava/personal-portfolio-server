8d6295a9c03c41ce2d5c42eff008c65674363be3
nikachitava <chitava18.official@gmail.com>
2024-12-17 16:38:42 +0400

    changes for deployment

--- index.js
+++ index.js
@@ -30,15 +30,17 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 mongoose
-	.connect(process.env.MONGO_URI)
+	.connect(
+		"mongodb+srv://chitava18official:GjOJ9FlDCQJvXy6d@portfolio-server.w0pat.mongodb.net/?retryWrites=true&w=majority&appName=portfolio-server"
+	)
 	.then(() => {
 		console.log("Connected to database");
 		// app.listen(PORT, () => {
 		// 	console.log(`Server running on port ${PORT}`);
 		// });
 	})
-	.catch(() => {
-		console.log("Connected failedd");
+	.catch((error) => {
+		console.log("Connected failedd", error);
 	});
 
 app.listen(PORT, () => {
